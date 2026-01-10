bc4107790ef6add1cdc820dcd4dde92bb8326978
Manish <manishlodhi0224@gmail.com>
2026-01-10 13:41:58 +0530

    landing layout defined

---
+++ src/components/LandingLayout.jsx
@@ -0,0 +1,26 @@
+import { Outlet } from 'react-router-dom';
+import Header from './header/Header';
+import
+
+function LandingLayout({ }) {
+    return (
+        <>
+
+            <div >
+                {/* Navigation Header */}
+                <Header />
+
+                {/* Main Content */}
+                <main >
+                    <Outlet />
+                </main>
+
+                {/* Footer */}
+                <Footer />
+            </div>
+
+        </>
+    )
+}
+
+export default LandingLayout;
