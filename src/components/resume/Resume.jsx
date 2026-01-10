8cdd2569df514288700cfbf4088188fb5a508bc9
Manish <manishlodhi0224@gmail.com>
2026-01-10 13:44:01 +0530

    resume component

---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,11 @@
+function Resume() {
+    return (
+        <div>
+            <p className="text-3xl text-cyan-300 text-center">
+                hello from resume 
+            </p>
+        </div>
+    )
+}
+
+export default Resume;
