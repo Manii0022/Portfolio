3046d90ee8c56c2315133ff36cbfed514ffa843e
Manish <manishlodhi0224@gmail.com>
2026-01-10 13:41:40 +0530

    routes creation

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createRoutesFromElements } from 'react-router-dom'
-import { Route } from 'lucide-react'
-
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Projects from './components/project/Projects'
+import Resume from './components/resume/Resume'
+import Particle from './components/Particle'
 const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<LandingPage />}>
-    <Route index={true} element={<Home />} />
+  <Route path='/' element={<Home />}>
     <Route path='about' element={<About />} />
     <Route path='projects' element={<Projects />} />
     <Route path='resume' element={<Resume />} />
   </Route>
-
 ))
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <>
+    <Particle />
+    <div style={{ position: "relative", zIndex: 0, width: "100%" }}>
+      <RouterProvider router={router} />
+    </div>
+  </>
+
+
 )
