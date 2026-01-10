6858cbb4ee7694f8ccac6c45c83e3f0242551b69
Manish <manishlodhi0224@gmail.com>
2026-01-10 13:43:50 +0530

    projects component

---
+++ src/components/project/Projects.jsx
@@ -0,0 +1,13 @@
+function Project(){
+    return (
+        <>
+        <div>
+            <p className="text-3xl text-blue-500 text-center">
+                hello from project
+            </p>
+        </div>
+        </>
+    )
+}
+
+export default Project;
