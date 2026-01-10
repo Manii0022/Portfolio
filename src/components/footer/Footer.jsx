568b030e9ceb534b3d4cdb32779f553a79a1b71d
Manish <manishlodhi0224@gmail.com>
2026-01-10 13:42:26 +0530

    footer defined

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <>
+
+            <div>
+                <p className="text-5xl text-purple-600 text-center"></p>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
