aeb8880998237d7289f6a7b1dd5b4a578e2ede03
Manish <manishlodhi0224@gmail.com>
2026-01-10 13:42:33 +0530

    header defined

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <>
+
+            <div>
+                <p className="text-4xl text-amber-600 text-center">Hello from header </p>
+            </div>
+        </>
+    )
+}
+
+export default Header;
