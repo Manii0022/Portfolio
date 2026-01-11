98307ece122425eb17db0f63444006f56a8f8a84
Manish <manishlodhi0224@gmail.com>
2026-01-11 12:58:48 +0530

    header code

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,47 @@
+import { NavLink } from "react-router-dom";
+import { House, FolderGit2, FileText, User } from 'lucide-react';
+
 function Header() {
+
+    const navItems = [
+        { path: '/', label: "Home", icon: 'House' },
+        { path: 'about', label: "About", icon: 'User' },
+        { path: 'projects', label: "Projects", icon: 'FolderGit2' },
+        { path: 'resume', label: "Resume", icon: "FileText" },
+    ]
     return (
         <>
 
-            <div>
-                <p className="text-4xl text-amber-600 text-center">Hello from header </p>
-            </div>
+            <nav className="hidden md:flex items-center space-x-8">
+                {navItems.map((item) => (
+                    <NavLink
+                        key={item.path}
+                        to={item.path}
+                        className={({ isActive }) => `text-sm font-medium transition-colors 
+                                        ${isActive ? "text-blue-600" : "text-slate-600 hover:text-slate-900"}`}
+                    >
+                        <div className="group flex flex-col text-white">
+                            <div className="flex items-center">
+                                <div>
+                                    {item.icon === 'House' && <House />}
+                                    {item.icon === 'FolderGit2' && <FolderGit2 />}
+                                    {item.icon === 'FileText' && <FileText />}
+                                    {item.icon === 'User' && <User />}
+                                </div>
+
+                                <div className="pl-2 text-[20px]">
+                                    {item.label}
+                                </div>
+                            </div>
+                            
+                            <div className={`h-1 rounded-4xl group-hover:bg-[#be50f4] transform origin-left scale-x-0 group-hover:scale-x-100 transition-transform duration-400`}></div>
+
+
+                        </div>
+
+                    </NavLink>
+                ))}
+            </nav>
         </>
     )
 }
