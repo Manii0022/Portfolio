bf107f6cdade30c1a291b510fd59e29c59b12904
Manish <manishlodhi0224@gmail.com>
2026-01-18 21:00:50 +0530

    more code

--- src/components/LandingLayout.jsx
+++ src/components/LandingLayout.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import Header from './header/Header';
 import Footer from './footer/Footer';
+import Header2 from './header/Header2.jsx'
+
 
 function LandingLayout() {
     return (
@@ -8,10 +10,10 @@ function LandingLayout() {
 
             <div >
                 {/* Navigation Header */}
-                <Header />
+                <Header2 />
 
                 {/* Main Content */}
-                <main >
+                <main className='pt-20'>
                     <Outlet />
                 </main>
 
