1d34d96fe21c2ca36ce465acbe68d34742079783
Manish <manishlodhi0224@gmail.com>
2026-01-18 21:01:28 +0530

    content addition

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,7 +8,36 @@ function Home() {
   return (
 
     <>
-      <Type />
+      <div>
+        <div>
+          <h1 style={{ paddingBottom: 15 }} className="heading">
+            Hi There!{" "}
+            <span className="wave" role="img" aria-labelledby="wave">
+              👋🏻
+            </span>
+          </h1>
+
+          <h1 className="heading-name">
+            I'M
+            <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+          </h1>
+
+          <div style={{ padding: 50, textAlign: "left" }}>
+            <Type />
+          </div>
+
+        </div>
+
+        <div>
+          <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+        </div>
+      </div>
+
     </>
 
   );
